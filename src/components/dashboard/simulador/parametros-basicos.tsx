b38eacb55dece4228855bd614d23c1c99aec915e
pa-tiq <patrickdsilva99@gmail.com>
2024-06-18 22:13:56 -0300

    wip

--- src/components/dashboard/simulador/parametros-basicos.tsx
+++ src/components/dashboard/simulador/parametros-basicos.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import { Slider, Typography } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
 import { StyledEngineProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Unstable_Grid2';
+
 import SliderInput from '@/components/core/sliderInput';
+
+import { IdadeAtual } from './idade-atual';
 
 export function ParametrosBasicos(): React.JSX.Element {
   return (
     <Grid container spacing={3}>
       <Grid lg={3} sm={6} xs={12}>
-        <StyledEngineProvider injectFirst>
-          <SliderInput />
-        </StyledEngineProvider>
+        <IdadeAtual sx={{ height: '100%' }} value="1.6k" />
       </Grid>
       <Grid lg={3} sm={6} xs={12}></Grid>
       <Grid lg={3} sm={6} xs={12}></Grid>
